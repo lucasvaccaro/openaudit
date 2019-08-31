2fb81e4cb8641b6c4019d1c697d9dab89006cdc0
Lucas <lucas-vaccaro@outlook.com>
2019-08-31 15:39:56 -0300

    Done routes compute collector

--- openaudit/compute_collector.py
+++ openaudit/compute_collector.py
@@ -17,12 +17,11 @@ class ComputeCollector():
         pass
 
     @abstractmethod
-    def parseData(self, data):
-        pass
-
-    @abstractmethod
     def saveData(self, data, snapshot_id):
         pass
+
+    def runCmd(self, cmd):
+        return subprocess.check_output(cmd, shell=True)
 
 
 class IsolationComputeCollector(ComputeCollector):
@@ -30,8 +29,8 @@ class IsolationComputeCollector(ComputeCollector):
     Returns ouput of OS commands 'hostname' and 'virsh'
     """ 
     def getData(self):
-        host = subprocess.check_output("hostname", shell=True)
-        vms = subprocess.check_output("virsh list --uuid", shell=True)
+        host = self.runCmd("hostname")
+        vms = self.runCmd("virsh list --uuid")
         return (host, vms)
 
     """
@@ -74,11 +73,72 @@ class SecurityGroupsComputeCollector(ComputeCollector):
 
 class RoutesComputeCollector(ComputeCollector):
     def getData(self):
-        pass
+        # Get dict of routers
+        routers = self.getRouters()
+        for uuid in routers:
+            # Get routers` configuration
+            self.getInet(routers, uuid)
+            self.getRoutes(routers, uuid)
+        return routers
 
-    def parseData(self, data):
-        pass
+    def getRouters(self, lines = None):
+        """
+        Gets all the routers in the host
+        Returns a dictionary where the key is the router`s uuid
+        """
+        # Get namespaces
+        if lines is None:
+            lines = self.runCmd("ip netns").strip().splitlines()
+        # Build map of routers where the key is the uuid
+        routers = {}
+        for line in lines:
+            net = line.strip()
+            if net.startswith("qrouter"):
+                uuid = net[8:44]
+                routers[uuid] = {}
+        return routers
 
+    def getInet(self, routers, uuid, lines = None):
+        """
+        Gets the IP address of each interface of a router
+        """
+        if lines is None:
+            cmd = "sudo ip netns exec qrouter-" + uuid + " ip -4 addr | grep inet"
+            lines = self.runCmd(cmd).strip().splitlines()
+        lines.pop(0) # Remove loopback
+        for line in lines:
+            elements = line.strip().split()
+            inet = elements[1]
+            iface = elements[6]
+            if not iface in routers[uuid]:
+                routers[uuid][iface] = {}
+            routers[uuid][iface].update({"inet": inet})
+
+    def getRoutes(self, routers, uuid, lines = None):
+        """
+        Gets the routing table of a router
+        """
+        if lines is None:
+            cmd = "sudo ip netns exec qrouter-" + uuid + " ip route"
+            lines = self.runCmd(cmd).strip().splitlines()
+        default_gw = lines.pop(0).split()[2] # Get default gateway
+        for line in lines:
+            elements = line.strip().split()
+            cidr = elements[0]
+            iface = elements[2]
+            src = elements[8]
+            if not iface in routers[uuid]:
+                routers[uuid][iface] = {}
+            routers[uuid][iface].update({"cidr": cidr, "src": src, "default_gw": default_gw})
+    
     def saveData(self, data, snapshot_id):
-        pass
-
+        sql_insert = "INSERT INTO openaudit.snapshot_routes_compute (uuid, iface, inet, cidr, src, default_gw, snapshot_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor = self.db_conn.cursor()
+        rowcount = 0
+        for uuid in data:
+            for iface, config in data[uuid].items():
+                values = (uuid, iface, config["inet"], config["cidr"], config["src"], config["default_gw"], snapshot_id)
+                cursor.execute(sql_insert, values)
+                rowcount += cursor.rowcount
+        self.db_conn.commit()
+        return rowcount
--- tests/compute_collector_test.py
+++ tests/compute_collector_test.py
@@ -18,6 +18,26 @@ class IsolationComputeCollectorTest(unittest.TestCase):
         msg = "Result: " + str(res) + " Should be " + str(size)
         self.assertEqual(res, size, msg)
 
+class RoutesComputeCollectorTest(unittest.TestCase):
+
+    c = compute_collector.RoutesComputeCollector()
+
+    def test(self):
+        netns = ["   qrouter-5ed262ef-920f-4e51-a0a1-5021f1671288 (id: 3)", " qdhcp-4c1b2a8f-35e1-4a1c-a434-9c790f090b02 (id: 2)"]
+        inets = ["  inet 127.0.0.1/8 scope host lo", "inet 172.24.4.94/24 brd 172.24.4.255 scope global qg-f94db071-e1", "inet 10.2.0.1/16 brd 10.2.255.255 scope global qr-c65e73a3-89"]
+        routes = ["  default via 172.24.4.1 dev qg-f94db071-e1", "  10.2.0.0/16 dev qr-c65e73a3-89 proto kernel scope link src 10.2.0.1", "172.24.4.0/24 dev qg-f94db071-e1 proto kernel scope link src 172.24.4.94"]
+        
+        routers = self.c.getRouters(netns)
+        for uuid in routers:
+            self.c.getInet(routers, uuid, inets)
+            self.c.getRoutes(routers, uuid, routes)
+
+        snapshot_id = 1
+        res = self.c.saveData(routers, snapshot_id)
+        size = 2
+        msg = "Result: " + str(res) + " Should be " + str(size)
+        self.assertEqual(res, size, msg)
+
 
 if __name__ == '__main__':
     unittest.main()
